3e126c2b8bdca94c8090bdc34ff8e7500a95cb78
shiva <ghassemzadeh.sh@gmail.com>
2025-10-02 22:40:28 +0200

    add ui navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import Navabr from "./components/navbar/Navbar";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navabr/>
+        {children}</body>
     </html>
   );
 }
