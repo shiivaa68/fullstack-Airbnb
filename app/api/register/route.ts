ce7da1656f59a410a43d9fce32eb41da4a634dd0
shiva <ghassemzadeh.sh@gmail.com>
2025-10-09 10:46:04 +0200

    add first post route for register user

---
+++ app/api/register/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, name, password } = body;
+
+    if (!email || !name || !password) {
+      return new NextResponse("Missing fields", { status: 400 });
+    }
+
+    // ✅ Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return new NextResponse("Email already registered", { status: 409 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("REGISTER_ERROR", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
