4086f4bc6ce5522cab6ad076a0820094f493d340
shiva <ghassemzadeh.sh@gmail.com>
2025-10-16 22:27:17 +0200

    Favorites screen (Loading favorites with server component)

---
+++ app/favorites/page.tsx
@@ -0,0 +1,29 @@
+import EmptyState from "../components/EmptyState";
+import Clientonly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
+import FavoritesClient from "./FavoritesClient";
+
+const ListingPage = async () => {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <Clientonly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorites listings"
+        />
+      </Clientonly>
+    );
+  }
+  return (
+    <Clientonly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </Clientonly>
+  );
+};
+
+export default ListingPage;
